813baba4ed2bf4cdc7e3e15ce0a2b649b3cee669
Kaushik Chaubal <kaushikchaubal@gmail.com>
2017-02-27 19:07:29 +0000

    Making rest call using services

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service'
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [ UsersService ]
 })
 export class UserComponent implements OnInit {
+  users: User[]
 
-  constructor() { }
+  constructor(private usersService: UsersService) {
+    this.usersService.getUsersFromJsonPlaceholder().subscribe( users => {
+      this.users = users;
+    });
+  }
 
   ngOnInit() {
   }
 
+}
+
+interface User {
+  id : number;
+  name : string;
+  username : string;
+  email : string;
+  address: address;
+}
+
+interface address {
+  street : string;
+  suite:  string;
+  city:  string;
+  zipcode :  string;
+  geo : geo;
+}
+
+interface geo {
+  lat : string;
+  lng : string;
 }
